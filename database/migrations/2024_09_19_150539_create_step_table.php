bbc29340292903a52c40d1c45b6150fa3cf0de5d
Philippart Cyril <70472789+cyril-philippart@users.noreply.github.com>
2024-09-19 17:15:23 +0200

    create model/controller/migration

---
+++ database/migrations/2024_09_19_150539_create_step_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('step', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 100);
+            $table->string('number', 100);
+            $table->string('departure', 100);
+            $table->string('arrival', 100);
+            $table->string('seat', 100)->nullable();
+            $table->string('gate', 100);
+            $table->string('baggage_drop', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('step');
+    }
+};
