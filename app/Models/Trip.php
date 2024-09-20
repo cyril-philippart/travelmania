4c4b2a947c512f9f268ce81cf5b5fe4766f0d593
Philippart Cyril <70472789+cyril-philippart@users.noreply.github.com>
2024-09-20 11:54:26 +0200

    add route trips

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Trip extends Model
 {
     use HasFactory;
+    protected $table = 'trip';
+
+    protected $fillable = [
+        'title',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Trip;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/voyages')->name('voyage.')->group(function() {
+    Route::get('/', function () {
+        return Trip::all();
+    })->name('index');
+    
+    Route::get('/{slug}', function (string $slug) {
+        $trip = Trip::where('title', $slug)->first();
+        return $trip;
+    })->name('show');
+});
+
+
+
 
 Route::get('/dashboard')->uses([\App\Http\Controllers\DashboardController::class, 'index']);
