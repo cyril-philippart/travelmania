e7cf6c2841f7239a042521597ea92209f5e934b6
Philippart Cyril <70472789+cyril-philippart@users.noreply.github.com>
2024-09-25 17:00:46 +0200

    redirect route /

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('voyage.index');
 });
 
 Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
